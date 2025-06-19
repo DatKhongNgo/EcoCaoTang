23bcc906016769c71ec4f5552ec7e8fc3ab95daa
DatKhongNgo <163614109+DatKhongNgo@users.noreply.github.com>
2025-06-19 20:09:46 +0700

    Update wp-config.php

--- EcoCaoTang/wp-config.php
+++ EcoCaoTang/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'ecocaotang' );
+define( 'DB_NAME', 'railway' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'KWMfSQzfGyEdjorJeRHRGgExYTHCCNTs' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql.railway.internal:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
